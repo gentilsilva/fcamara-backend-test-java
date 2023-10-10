1686f4667e58edb4aa7846d14d81df287c61aa88
Gentil Silva <silvagentil.sg@gmail.com>
2023-10-09 21:05:08 -0300

    feat: Inicial config

---
+++ fcamaraparking/src/main/java/com/fcamaraparking/FcamaraparkingApplication.java
@@ -0,0 +1,13 @@
+package com.fcamaraparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcamaraparkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcamaraparkingApplication.class, args);
+    }
+
+}
---
+++ fcamaraparking/src/test/java/com/fcamaraparking/FcamaraparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcamaraparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcamaraparkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
